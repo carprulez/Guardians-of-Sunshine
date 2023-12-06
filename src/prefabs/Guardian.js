6cdff4b0c0fc49e284667ae153fe5cf8d0e1f2e4
carprulez <147204410+carprulez@users.noreply.github.com>
2023-12-05 16:47:07 -0800

    set up state machine

--- src/prefabs/Guardian.js
+++ src/prefabs/Guardian.js
@@ -1,6 +1,22 @@
 // Guardian prefab
 class Guardian extends Phaser.Physics.Arcade.Sprite {
-    constructor() {
-        
+    constructor(scene, x, y, texture, frame, direction) {
+        super(scene, x, y, texture, frame); // call Sprite parent class
+        scene.add.existing(this);           // add Guardian to existing scene
+        scene.physics.add.existing(this);   // add physics body to scene
+
+        this.body.setCollideWorldBounds(true);
+
+        // set Guardian properties
+        this.direction = direction;
+        this.guardianVelocity = 100 // in pixels
+
+        // initialize state machine managing guardian
+        scene.guardianFSM = new StateMachine('idle', {
+            idle: new IdleState(),
+            move: new MoveState(),
+            attack: new AttackState(),
+            jump: new JumpState(),
+        }, [scene, this])               // use this as arguments to keep scene and object context
     }
 }
