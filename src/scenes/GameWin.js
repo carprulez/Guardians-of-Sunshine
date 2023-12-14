46a39d60f679f8580ead23163c7301c5e8b4db12
carprulez <147204410+carprulez@users.noreply.github.com>
2023-12-14 08:57:57 -0800

    added win screen text

--- src/scenes/GameWin.js
+++ src/scenes/GameWin.js
@@ -6,6 +6,17 @@ class Win extends Phaser.Scene {
 
     create() {
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+        // adding text to get back to menu
+        let textConfig = {
+            fontFamily: 'Impact',
+            fontSize: '15px',
+            color: '#68BE16',
+            align: 'center',
+            fixedWidth: 0
+        }
+        this.add.text(centerX, h - 15, 'Use SPACE to go back to menu', textConfig).setOrigin(0.5);
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     update() {
